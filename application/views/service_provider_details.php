6af2121026055a4ad896017ac083584fec531116
shwethashree <shwethashreen07@gmail.com>
2018-07-31 15:29:56 +0530

    logout

--- application/views/service_provider_details.php
+++ application/views/service_provider_details.php
@@ -14,16 +14,19 @@
                 
               
               echo "
+              <form method='post'>
                <div class='col-md-3 services_display'>
                <div class='caption'>
-                        <p>$row->sp_details.</p>
-                        <p>$row->sp_price .</p>
-                        <p>$row->sp_date_slot.</p>
-                        <p>$row->sp_time_slot.</p>";
-
-                        if(isset($_POST['cust_id']))
+                        <p><b>Details</b> : $row->sp_details.</p>
+                        <p><b>Price </b>: $row->sp_price .</p>
+                        <p><b>Date slot </b>: $row->sp_date_slot.</p>
+                        <p><b>Time slot</b> : $row->sp_time_slot.</p>";
+                        echo "<input type='date' name='date' >";
+                        
+                        
+                        if(null !== ($this->session->userdata('user')))
                         {
-                        echo "<a href='payment/payment' style='float:right;color:#51627C;'>Book Now</a><br>";
+                        echo "<a href='../payment/payment_start' style='float:right;color:#51627C;'>Book Now</a><br>";
                         }
                        
                         else{
@@ -32,7 +35,9 @@
                         
 
               echo" </div>
-               </div>";
+               </div>
+               
+               </form>";
                
                
               }
